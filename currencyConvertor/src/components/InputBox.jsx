df64cdc01a374537b041dcf6c716b002b8d44e46
LepharamRamchiary <lepharamchiary@gmail.com>
2025-07-23 17:26:53 +0530

    complete the currency conveter project

--- currencyConvertor/src/components/InputBox.jsx
+++ currencyConvertor/src/components/InputBox.jsx
@@ -34,7 +34,7 @@ function InputBox({
           }
         />
       </div>
-      <div className="w-1/2 flex justify-end text-right">
+      <div className="w-1/2 justify-end text-right">
         <p className="text-black/40 mb-2 w-full">Currency Type</p>
         <select
           className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
