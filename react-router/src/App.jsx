ffa3755049d976628f9fda6677b296d26c21a4ac
LepharamRamchiary <lepharamchiary@gmail.com>
2025-07-24 16:56:55 +0530

    add about compoents and some simple routing

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
-   <div>
-    <h1 className='text-red-300 '>Hello</h1>
-   </div>
-  )
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
--- react-router/src/components/Home/Home.jsx
+++ react-router/src/components/Home/Home.jsx
@@ -32,12 +32,12 @@ export default function Home() {
                 </div>
 
                 <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
-                    <img className="w-96" src="https://i.ibb.co/5BCcDYB/Remote2.png" alt="image1" />
+                    <img className="w-96" src="https://media.istockphoto.com/id/1500502940/vector/young-man-sportswear-riding-a-bicycle-with-a-small-dog-in-a-basket.jpg?s=612x612&w=0&k=20&c=kki6_MmhRsfBOGID0EPcJXl6TwJnZ7jKo7d6C0Gw4H0=" alt="image1" />
                 </div>
             </aside>
 
             <div className="grid  place-items-center sm:mt-20">
-                <img className="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
+                <img className="sm:w-96 w-48" src="https://thumbs.dreamstime.com/b/lady-plucking-tea-leaves-plantation-coloring-page-black-white-line-art-illustration-perfect-book-depicting-woman-379664964.jpg" alt="image2" />
             </div>
 
             <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
