7d20ef60017c077c0f9ffeb52906a76e79d553e3
LepharamRamchiary <lepharamchiary@gmail.com>
2025-07-24 17:36:38 +0530

    add user and github componets and add api for github

---
+++ react-router/src/components/GitHub/GitHub.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+
+function GitHub() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/LepharamRamchiary")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("GitHub Followers:", data.followers);
+        setData(data);
+      });
+  }, []);
+
+  return (
+    <div className="text-center m-4 bg-gray-500 text-white p-4 text-3xl flex flex-col items-center gap-3">
+      GitHub Fllowers: {data.followers}
+      <img src={data.avatar_url} alt="Git picture"  width={300}/>
+      <p>Pubilc repo: {data.public_repos}</p>
+    </div>
+  );
+}
+
+export default GitHub;
--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -2,27 +2,50 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+import GitHub from './components/GitHub/GitHub.jsx'
+
+// one way to set up routing
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     children: [
+//       {
+//         path: '',
+//         element: <Home />
+//       },
+//       {
+//         path: 'about',
+//         element: <About />
+//       },
+//       {
+//         path: 'contact',
+//         element: <Contact />
+//       }
+//     ]
+//   }
+// ])
 
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '',
-        element: <Home />
-      },
-      {
-        path: 'about',
-        element: <About />
-      }
-    ]
-  }
-])
+
+// another way to set up routing
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+        <Route path='' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='user/:userid' element={<User />} />
+        <Route path='github' element={<GitHub />} />
+        <Route path='*' element={<div className='text-center m-4 bg-red-500 text-white p-4 text-3xl'>Page Not Found</div>} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
