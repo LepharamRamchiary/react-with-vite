74342eb2c761a63367ab52c3cb3e153d2358d7a9
LepharamRamchiary <lepharamchiary@gmail.com>
2025-07-30 14:34:59 +0530

    add project for todo using context api and local storage

---
+++ Todo-context-localStorage/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  
+
+  return (
+  <div className="h-screen w-screen flex items-center justify-center">
+    <h1 className="text-red-500">Hello</h1>
+  </div>
+  )
+}
+
+export default App
