31afe51542a39f4a95aba63205503b1a006e0c72
LepharamRamchiary <lepharamchiary@gmail.com>
2025-08-01 00:18:14 +0530

    create simple todo list sclice

---
+++ redux-toolkit/src/features/todo/todoSlice.js
@@ -0,0 +1,33 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [{ id: 1, text: "Helllo world" }],
+};
+
+export const todoSclice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    upadeteTodo: (state, action) => {
+      const { id, text } = action.payload;
+      const todo = state.todos.find((todo) => todo.id == id);
+      if (todo) {
+        todo.text = text;
+      }
+    },
+  },
+});
+
+export const { addTodo, removeTodo, upadeteTodo } = todoSclice.actions;
+
+export default todoSclice.reducer;
